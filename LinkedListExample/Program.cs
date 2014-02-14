ab87b8a5592ba0b00611e34f8e1871561579c18d
Alpinfish <jason.astuto@gmail.com>
2014-02-14 12:26:41 -0800

    initial commit

---
+++ LinkedListExample/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SinglyLinkedListExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SinglyLinkedList singlylinkedlist = new SinglyLinkedList();
+
+            singlylinkedlist.Add("A");
+            singlylinkedlist.Add("B");
+            singlylinkedlist.Add("C");
+            singlylinkedlist.Add("D");
+            singlylinkedlist.Add("E");
+            singlylinkedlist.Add("F");
+            singlylinkedlist.Add("G");
+            singlylinkedlist.Add("H");
+
+            singlylinkedlist.PrintLinkedListContent();
+            
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Deleting node 8");
+
+            singlylinkedlist.Delete(8);
+
+            Console.WriteLine();
+            singlylinkedlist.PrintLinkedListContent();
+            
+            Console.WriteLine();
+            Console.WriteLine(); 
+            Console.WriteLine("Position 5: " + singlylinkedlist.Retrieve(5).NodeContent);
+
+            
+            Console.WriteLine();
+            Console.WriteLine("Deleting node 1");
+
+            singlylinkedlist.Delete(1);
+
+            Console.WriteLine();
+            Console.WriteLine("Position 5: " + singlylinkedlist.Retrieve(5).NodeContent);
+
+            Console.WriteLine();
+            singlylinkedlist.PrintLinkedListContent();
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ LinkedListExample/SinglyLinkedList.cs
@@ -0,0 +1,117 @@
+﻿using System;
+
+
+namespace SinglyLinkedListExample
+{
+    class SinglyLinkedList
+    {
+
+        private Node head;
+        private Node current;
+        public int Size { get; set; }
+
+        public class Node
+        {
+            public object NodeContent;
+            public Node Next;
+        }
+
+        public SinglyLinkedList()
+        {
+            Size = 0;
+            head = null;
+        }
+
+
+
+        public void PrintLinkedListContent()
+        {
+            Node tempNode = head;
+
+            while (tempNode != null)
+            {
+                Console.Write(tempNode.NodeContent);
+                tempNode = tempNode.Next;
+            }
+        }
+
+        public void Add(object content)
+        {
+            Size++;
+            var node = new Node()
+            {
+                NodeContent = content
+            };
+
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                current.Next = node;
+            }
+
+            current = node;
+        }
+
+        public void Delete(int position)
+        {
+            Node previousNode = null;
+            Node currentNode = head;
+
+            int count = 1;
+            while (currentNode != null)
+            {
+                while (count <= Size)
+                {
+                    if (count == position)
+                    {
+                        if (previousNode == null)
+                        {
+                            head = currentNode.Next;
+                            current = null;
+                        }
+                        else
+                        {
+                            previousNode.Next = currentNode.Next;
+                            currentNode = null;                            
+                        }
+                    }
+                    else
+                    {
+                        previousNode = currentNode;
+                    }
+
+                    if (currentNode != null)
+                    {
+                        currentNode = currentNode.Next;
+                    }
+
+                    count++;
+                }
+            }
+        }
+
+        public Node Retrieve(int Position)
+        {
+            Node tempNode = head;
+            Node retNode = null;
+            int count = 0;
+
+            while (tempNode != null)
+            {
+                if (count == Position - 1)
+                {
+                    retNode = tempNode;
+                    break;
+                }
+                count++;
+                tempNode = tempNode.Next;
+            }
+
+            return retNode;
+        }
+
+    }
+}
